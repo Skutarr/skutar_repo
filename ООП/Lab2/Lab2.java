1d12c7d234a8d8a91625cee1ece835f162f9461e
Skutarr <rom65904411@yandex.ru>
2020-01-10 23:28:47 +0300

    Cummit
    cummit3

---
+++ ООП/Lab2/Lab2.java
@@ -0,0 +1,26 @@
+public class Lab1 {
+    double area;
+
+    public static void main(String[] args) {
+
+        /**
+         * СОздание объектов класса
+         **/
+        Point3d first3dPoint = new Point3d(1, 2, 3);
+        Point3d second3dPoint = new Point3d(1, 2, 3);
+        Point3d third3dPoint = new Point3d(5, 2, 3);
+        first3dPoint.comparePoints(first3dPoint,second3dPoint,third3dPoint);
+
+    }
+
+   /** static double compureArea() {
+        double area1;
+        Point3d newfirst3dPoint = new Point3d(1, 2, 3);
+        Point3d newsecond3dPoint = new Point3d(4, 5, 6);
+        Point3d newthird3dPoint = new Point3d(9, 8, 7);
+**/
+
+    }
+
+
+
