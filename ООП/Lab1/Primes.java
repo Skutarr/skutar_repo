e106eee8d9dadb8ae809f1a80c985e0bc744071d
Skutarr <rom65904411@yandex.ru>
2020-01-10 23:36:13 +0300

    1
    1

---
+++ ООП/Lab1/Primes.java
@@ -0,0 +1,15 @@
+public class Primes {
+    public static void main(String[] args) {
+        int x, y;
+        for (x = 1; x <101; x++) {
+            y = 0;
+            for (int i = 1; i <= x; i++) {
+                if (x % i == 0)
+                    y++;
+            }
+            if (y <= 2)
+                System.out.println(x);
+
+        }
+    }
+}
